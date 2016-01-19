4df061a4d0826f454d1bd75aade7d12fa03074e2
ŁukaszKomajda <l.komajda@wp.pl>
2016-01-19 16:24:31 +0100

    funkcja pobieranie xml

--- scripts/tableData.js
+++ scripts/tableData.js
@@ -2,42 +2,20 @@
  * Created by lukaszk on 19.01.16.
  */
 
-function pobierzDaneJson(){
-
-
-    $.ajax({
-        url: 'http://www.nbp.pl/kursy/xml/c011z160119.xml',
-        dataType:'xml',
-        success:function(response){
-            var kontener = $('#posty');
-            response.forEach(function(post){
-                var kodWaluty = $(this).find('kod_waluty').text();
-                kontener.html('<p>'+kodWaluty+'</p>')
-            });
-        }
+$(document).ready(function(){
+//the variable to be appended here
+    $("#Autom").append("<ul></ul>");
+});
+$.ajax({
+    type: "GET",
+    url: "cars.xml",
+    dataType: "xml",
+    success: function(xml) {
+    }
+    $(xml).find('car').each(function(){
+        var Titles = $(this).find('Title').text();
+        var Manufacturers = $(this).find('Manufacturer').text();
+        $("<li></li>").html(Titles + "-" + Manufacturers).appendTo("#Autom ul");
     });
-};
 
-
-
-
-//
-//$(document).ready(function(){
-////the variable to be appended here
-//    $("#Autom").append("<ul></ul>");
-//});
-//$.ajax({
-//    type: "GET",
-//    url: "cars.xml",
-//    dataType: "xml",
-//    success: function(xml) {
-//    }
-//    $(xml).find('car').each(function(){
-//        var Titles = $(this).find('Title').text();
-//        var Manufacturers = $(this).find('Manufacturer').text();
-//        $("<li></li>").html(Titles + "-" + Manufacturers).appendTo("#Autom ul");
-//    });
-//error: function() {
-//    alert("The XML File could not be processed correctly.");
-//}
-//});
+})
