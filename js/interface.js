c676d7bcbd36ba7ea7760654262e5b8ad549ca0d
Tomasz Nastały <nastalytomasz@gmail.com>
2016-01-19 21:39:00 +0100

    interface

--- js/interface.js
+++ js/interface.js
@@ -1,3 +1,19 @@
 /**
  * Created by Tomasz on 19.01.2016.
  */
+$(document).ready(function(){
+
+    $( ".trigger-slide" ).click(function() {
+        $('.slide-list-right').animate({
+            right: 0
+        }, 400)
+    });
+
+    $( ".user-settings" ).click(function() {
+        $('.user-settings-dropdown').animate({
+            top: 50
+        }, 400)
+    });
+
+});
+
