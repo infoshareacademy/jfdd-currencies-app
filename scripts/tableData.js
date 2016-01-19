0e0996d272331f3ac1180e19fa85d8e39d0d9111
ŁukaszKomajda <l.komajda@wp.pl>
2016-01-19 17:04:09 +0100

    pobieranie danych z xml

--- scripts/tableData.js
+++ scripts/tableData.js
@@ -2,20 +2,41 @@
  * Created by lukaszk on 19.01.16.
  */
 
-$(document).ready(function(){
-//the variable to be appended here
-    $("#Autom").append("<ul></ul>");
-});
-$.ajax({
-    type: "GET",
-    url: "cars.xml",
-    dataType: "xml",
-    success: function(xml) {
-    }
-    $(xml).find('car').each(function(){
-        var Titles = $(this).find('Title').text();
-        var Manufacturers = $(this).find('Manufacturer').text();
-        $("<li></li>").html(Titles + "-" + Manufacturers).appendTo("#Autom ul");
+$(document).ready(function () {
+
+    $("#post").append("<ul></ul>");
+
+    $.ajax({
+
+        type: "GET",
+
+        url: "xml/kurs.xml",
+
+        dataType: "xml",
+
+        success: function (xml) {
+
+            $(xml).find('pozycja').each(function () {
+
+                var nazwaWaluty = $(this).find('nazwa_waluty').text();
+
+                var przelicznik = $(this).find('przelicznik').text();
+                var kodWaluty = $(this).find('kod_waluty').text();
+                var kursKupna = $(this).find('kurs_kupna').text();
+                var kursSprzedazy = $(this).find('kurs_sprzedazy').text();
+
+                $("<li></li>").html(nazwaWaluty + ", " + kodWaluty+", " +kursKupna+", " +kursSprzedazy).appendTo("#post");
+
+            });
+
+        },
+
+        error: function () {
+
+            alert("An error occurred while processing XML file.");
+
+        }
+
     });
 
-})
+});
