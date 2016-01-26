e01f4987d570063b79bcd5947eeb1730ee62b199
ŁukaszKomajda <l.komajda@wp.pl>
2016-01-26 17:16:14 +0100

    zapamietywanie wybranych walut

--- js/tableData.js
+++ js/tableData.js
@@ -3,19 +3,30 @@
  */
 
 $(document).ready(function () {
+    var leftSelectValue = localStorage.getItem("selectLeft");
+    var rightSelectValue = localStorage.getItem("selectRight");
+    var centerSelectValue = localStorage.getItem("selectCenter");
 
-    $(".left-select").after("<table class='table table-striped table-bordered'><thead><tr><th>Waluta</th><th>Kod waluty</th><th>Kurs kupna</th><th>Kurs sprzedaży</th></tr></thead><tr class='edit-tr'></tr></table>");
-    var leftSelect = localStorage.getItem("selectLeft");
-    var rightSelect = localStorage.getItem("selectRight");
-    var centerSelect = localStorage.getItem("selectCenter");
+    buildSelectCurrency('id="selectLeft" ',leftSelectValue,$('#selectLeft').parents('section')[0]);
+    buildSelectCurrency('id="selectRight" ',rightSelectValue,$('#selectRight').parents('section')[0]);
+    buildSelectCurrency('id="selectCenter" ',centerSelectValue,$('#selectCenter').parents('section')[0]);
 
-    actualizeTableData(leftSelect, $('#selectLeft').parents('section')[0]);
-    actualizeTableData(rightSelect, $('#selectRight').parents('section')[0]);
-    actualizeTableData(centerSelect, $('#selectCenter').parents('section')[0]);
+    $(".selectCur").after("<table class='table table-striped table-bordered'><thead><tr><th>Waluta</th><th>Kod waluty</th><th>Kurs kupna</th><th>Kurs sprzedaży</th></tr></thead><tr class='edit-tr'></tr></table>");
+
+
+
+
+
+    actualizeTableData(leftSelectValue, $('#selectLeft').parents('section')[0]);
+    actualizeTableData(rightSelectValue, $('#selectRight').parents('section')[0]);
+    actualizeTableData(centerSelectValue, $('#selectCenter').parents('section')[0]);
+
+
 
     $("select").change(function() {
         actualizeTableData($(this)[0].selectedIndex, $(this).parents('section')[0]);
         addLocalStorage(this.id,$(this)[0].selectedIndex);
+
     });
 
 });
@@ -53,4 +64,21 @@ function actualizeTableData (selectedOption, container){
 
 function addLocalStorage(selectId,selectedOption){
     localStorage.setItem(selectId, selectedOption);
+}
+
+
+function buildSelectCurrency(selectId, selectedOption, container) {
+    var buildSelect = '<select ' + selectId + ' class="form-control selectCur">';
+    var currency = ['Dolar amerykański', 'Dolar australijski', 'Dolar kanadyjski', 'Euro', 'Forint (Węgry)', 'Frank szwajcarski', 'Funt szterling', 'Jen (Japonia)', 'Korona czeska', 'Korona duńska', 'Korona norweska', 'Korona szwedzka', 'SDR (MFW)'];
+
+    for (var countCurrency = 0; countCurrency <= 12; countCurrency++) {
+        if (selectedOption == countCurrency) {
+            buildSelect += '<option selected>' + currency[countCurrency] + '</option>';
+        }
+        else {
+            buildSelect += '<option >' + currency[countCurrency] + '</option>';
+        }
+    }
+    buildSelect += '</select>';
+    $(container).prepend(buildSelect);
 }
