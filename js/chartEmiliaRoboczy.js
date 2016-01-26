fd842389c4661c24632f05bdfc20d6bd36fbf467
Emilia Kamrowska <emilia.kamrowska@o2.pl>
2016-01-27 00:04:57 +0100

    dane walut w mxl, filtrowanie po datach

--- js/chartEmiliaRoboczy.js
+++ js/chartEmiliaRoboczy.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
 
     var wybranaData1 = 20100409;
-    var wybranaData2 = 20100701;
+    var wybranaData2 = 20100421;
+    var dateChartArray = [];
+    var rateChartArray = [];
 
 
     function pobierzDane(url) {
-        //$('#tresc').append('<ul></ul>');
+
+
         $.ajax({
             type: "GET",
             url: url,
             dataType: "xml",
+            //async: false,
             error: function () {
                 $('#tresc').text('wystąpil błąd');
             },
@@ -28,6 +32,8 @@ $(document).ready(function () {
                     });
                 });
 
+
+
                 var startPosition = data.indexOf(wybranaData1);
                 var stopPosition = data.indexOf(wybranaData2);
 
@@ -40,12 +46,43 @@ $(document).ready(function () {
 
 
             }
-
         });
-
     };
 
     pobierzDane("../xml/GBP.xml");
+
+
+
+
+    var lineChartData = {
+        labels : [20100409, 20100412, 20100413, 20100414, 20100415, 20100416, 20100419, 20100420, 20100421],
+        datasets : [
+            {
+                label: "My First dataset",
+                fillColor : "rgba(220,220,220,0.2)",
+                strokeColor : "rgba(220,220,220,1)",
+                pointColor : "rgba(220,220,220,1)",
+                pointStrokeColor : "#fff",
+                pointHighlightFill : "#fff",
+                pointHighlightStroke : "rgba(220,220,220,1)",
+                data : rateChartArray
+            }
+
+        ]
+
+    };
+
+    window.onload = function(){
+        var ctx = document.getElementById("canvas").getContext("2d");
+        window.myLine = new Chart(ctx).Line(lineChartData, {
+            responsive: true
+        });
+    }
+
+    console.log(Array.isArray(dateChartArray));
+    console.log(Array.isArray(rateChartArray));
+
+
 
 
 });
