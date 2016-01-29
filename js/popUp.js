e989f2ae778dd345bdc172e861cf2b596a522a84
emilia <emilia.kamrowska@o2.pl>
2016-01-29 15:54:00 +0100

    PopUp

---
+++ js/popUp.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('#facebookLogin').click(function()    {
+        $('.logInBackground').addClass('blockPopUp');
+        $('.logInPopUp').addClass('blockPopUp')
+    });
+
+    $('#googleLogin').click(function()    {
+        $('.logInBackground').addClass('blockPopUp');
+        $('.logInPopUp').addClass('blockPopUp')
+    });
+
+});
+
