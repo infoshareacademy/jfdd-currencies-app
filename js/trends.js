d1832438fd9d1c9c5db5ff0d9e3f08a7611f9cd9
tomasznastaly <nastalytomasz@gmail.com>
2016-02-09 17:13:51 +0100

    Merge remote-tracking branch 'origin/develop' into develop
    Conflicts:
    	index.html

--- js/trends.js
+++ js/trends.js
@@ -1,74 +1,57 @@
+$.fn.datepicker.defaults.format = "yyyy-mm-dd";
+var currency = ['Dolar amerykański', 'Dolar australijski', 'Dolar kanadyjski', 'Euro', 'Forint (Węgry)', 'Frank szwajcarski', 'Funt szterling', 'Jen (Japonia)', 'Korona czeska', 'Korona duńska', 'Korona norweska', 'Korona szwedzka', 'SDR (MFW)'];
+var currenciesShorts = ['USD','AUD','CAD','EUR','HUF','CHF','GBP','JPY','CZK','DKK','NOK','SEK','XDR'];
 
-var currencySelected,
-    currencyDataFirst,
+
+var currencyDataFirst,
     currencyDataSecond;
 
-$("#selectButton").change(function () {
-    currencySelected = $(this).val();
-});
 
 $("#trendsForm").submit(function (event) {
+
     event.preventDefault();
     currencyDataFirst = $('#dateOne').val();
     currencyDataSecond = $('#dateTwo').val();
-    var url = ('xml/' + currencySelected + '.xml');
-    fetchData(url);
-    debugger;
+    var rows = [];
+
+    $.when.apply($, currenciesShorts.map(function (item) {
+        var url = ('xml/' + item + '.xml');
+        return fetchData(url);
+    })).done(function () {
+        var results = $.makeArray(arguments);
+        console.log(results);
+
+       results.forEach(function(item){
+            $xml = $(item[0]);
+           rows.push({
+               name: $xml.find('nazwa').text(),
+               min: $xml.find('pozycja').filter(function () {
+                   var x = $(this).text().trim();
+                   return x === currencyDataFirst;
+
+               }).text(),
+               max: $xml.find('pozycja').filter(function () {
+                   return $(this).text().trim() === currencyDataSecond
+               }).text()
+           })
+
+       });
+
+        console.log(rows);
+
+
+    });
+
     return false;
 });
 
 
-new JsDatePick({
-    useMode: 2,
-    target: "data1",
-    isStripped: false,
-    selectedDate: {
-        year: 2000,
-        month: 1,
-        day: 3
-    },
-    yearsRange: new Array(2000, 2016),
-    limitToToday: false,
-    dateFormat: "%Y-%m-%d"
-});
 
-new JsDatePick({
-    useMode: 2,
-    target: "data2",
-    isStripped: false,
-    selectedDate: {
-        year: 2000,
-        month: 1,
-        day: 4
-    },
-    yearsRange: new Array(2000, 2016),
-    limitToToday: false,
-    dateFormat: "%Y-%m-%d"
-});
+function fetchData(url,minDate,maxDate) {
 
-function fetchData(url) {
-
-    $.ajax({
+    return $.ajax({
         type: "GET",
         url: url,
-        dataType: "xml",
-        success: function (response) {
-
-            var data = [];
-
-
-            $(response).find('pozycja').each(function () {
-
-                $(this).find('data').each(function () {
-                    data.push($(this).text());
-                    console.log(data);
-                });
-            });
-
-
-        }
-
-    }).then(function () {
-
-    });
+        dataType: "xml"
+        });
 }
