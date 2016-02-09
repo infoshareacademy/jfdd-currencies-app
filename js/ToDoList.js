19a0f5c155de49608fef77515107251f89740d61
Emilia Kamrowska <emilia.kamrowska@o2.pl>
2016-02-09 23:02:07 +0100

    changing status, moving tasks  - done

--- js/ToDoList.js
+++ js/ToDoList.js
@@ -1,33 +1,62 @@
 (function () {
     var app = angular.module('Planer', []);
 
-    var statuses = {
-        isPlanned: {},
-        isDone: {},
-        isAnnuled: {}
-    };
+    //var statuses = {
+    //    isPlanned: {},
+    //    isDone: {},
+    //    isAnnuled: {}
+    //};
 
     app.controller('ToDoList', function ($scope) {
         $scope.taskList = [
-            {date: '2016-01-14', task: 'Sprzedać 300 USD', status: statuses.isDone},
-            {date: '2016-02-01', task: 'Sprzedać 1000 JPY, kupić 200 USD', status: statuses.isDone},
-            {date: '2016-02-01', task: 'Sprzedać 2000 JPY, kupić 1500 USD', status: statuses.isPlanned},
-            {date: '2016-02-01', task: 'Sprzedać 1000 JPY, kupić 700 USD', status: statuses.isPlanned},
-            {date: '2016-02-01', task: 'Sprzedać 200 JPY, kupić 500 USD', status: statuses.isAnnuled},
-            {date: '2016-02-20', task: 'Kupić 2000 EUR 300 USD', status: statuses.isAnnuled}
+            {date: '2016-01-14', task: 'Sprzedać 300 USD', status: "isDone"},
+            {date: '2016-02-01', task: 'Sprzedać 1000 JPY, kupić 200 USD', status: "isDone"},
+            {date: '2016-02-01', task: 'Sprzedać 2000 JPY, kupić 1500 USD', status: "isPlanned"},
+            {date: '2016-02-01', task: 'Sprzedać 1000 JPY, kupić 700 USD', status: "isPlanned"},
+            {date: '2016-02-01', task: 'Sprzedać 200 JPY, kupić 500 USD', status: "isAnnuled"},
+            {date: '2016-02-20', task: 'Kupić 2000 EUR 300 USD', status: "isAnnuled"}
         ];
-        var today = '2016-02-04';
 
-        $scope.newItem = {date: '', task: ''};
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1;
+        var yyyy = today.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        }
+
+        if(mm<10) {
+            mm='0'+mm
+        }
+        today = yyyy+'-'+mm+'-'+dd;
+
+
+        $scope.newItem = {date: '', task: '', status:"isPlanned"};
 
         $scope.addItem = function (item) {
             item.date = item.date || today;
             if (item.task) {
                 var newItem = angular.copy(item);
-                newItem.status=statuses.isPlanned;
+                //newItem.status=statuses.isPlanned;
                 $scope.taskList.unshift(newItem);
                 console.log($scope.taskList)
             }
+        };
+        $scope.removeItem = function (item) {
+            var idx = $scope.taskList.indexOf(item);
+            $scope.taskList.splice(idx, 1);
+        };
+
+        $scope.statusIntoDone = function(item)  {
+            item.status = "isDone";
+        };
+        $scope.statusIntoPlanned = function(item)  {
+            item.status = "isPlanned";
+            item.date = today;
+        };
+        $scope.statusIntoAnnuled = function(item)  {
+            item.status = "isAnnuled";
         };
 
     }).directive('changeList', function () {
@@ -38,19 +67,19 @@
     }).filter('doneTasks', function () {
         return function (items) {
             return items.filter(function (item) {
-                return item.status === statuses.isDone;
+                return item.status === "isDone";
             });
         }
     }).filter('plannedTasks', function () {
         return function (items) {
             return items.filter(function (item) {
-                return item.status === statuses.isPlanned;
+                return item.status === "isPlanned";
             });
         }
     }).filter('annuledTasks', function () {
         return function (items) {
             return items.filter(function (item) {
-                return item.status === statuses.isAnnuled;
+                return item.status === "isAnnuled";
             });
         }
     });
