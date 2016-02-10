d65c79305a760ab7f48b7233fad570d6d78a7baf
tomasznastaly <nastalytomasz@gmail.com>
2016-02-10 16:57:16 +0100

    Merge remote-tracking branch 'origin/develop' into develop
    Conflicts:
    	index.html

--- js/trends.js
+++ js/trends.js
@@ -1,7 +1,11 @@
 $.fn.datepicker.defaults.format = "yyyy-mm-dd";
 var currency = ['Dolar amerykański', 'Dolar australijski', 'Dolar kanadyjski', 'Euro', 'Forint (Węgry)', 'Frank szwajcarski', 'Funt szterling', 'Jen (Japonia)', 'Korona czeska', 'Korona duńska', 'Korona norweska', 'Korona szwedzka', 'SDR (MFW)'];
 var currenciesShorts = ['USD','AUD','CAD','EUR','HUF','CHF','GBP','JPY','CZK','DKK','NOK','SEK','XDR'];
+var currencyMap = {};
 
+currency.forEach(function (item, idx) {
+    currencyMap[currenciesShorts[idx]] = item;
+})
 
 var currencyDataFirst,
     currencyDataSecond;
@@ -11,6 +15,7 @@ $("#trendsForm").submit(function (event) {
 
     event.preventDefault();
     currencyDataFirst = $('#dateOne').val();
+    console.log(currencyDataFirst);
     currencyDataSecond = $('#dateTwo').val();
     var rows = [];
 
@@ -21,30 +26,36 @@ $("#trendsForm").submit(function (event) {
         var results = $.makeArray(arguments);
         console.log(results);
 
+        $('#trendsTable tbody').html('');
        results.forEach(function(item){
             $xml = $(item[0]);
-           rows.push({
-               name: $xml.find('nazwa').text(),
-               min: $xml.find('pozycja').filter(function () {
-                   var x = $(this).text().trim();
-                   return x === currencyDataFirst;
+            var row = {
 
-               }).text(),
+               name: $xml.find('nazwa').text(),
+
+                fullName: currencyMap[$xml.find('nazwa').text().trim()],
+
+               min: $xml.find('pozycja').filter(function () {
+                   return $(this).find('data').text().trim() === currencyDataFirst;
+               }).find('kurs').text(),
+
                max: $xml.find('pozycja').filter(function () {
-                   return $(this).text().trim() === currencyDataSecond
-               }).text()
-           })
+                   return $(this).find('data').text().trim() === currencyDataSecond;
+
+               }).find('kurs').text()
+           };
+
+           var percents = ((row.max / row.min)*100 - 100).toFixed(3);
+
+           var insertedRow = $('#trendsTable tbody').append('<tr><td>' + row.fullName + '</td>' + '<td>' + row.name
+               + '</td>' + '<td>' + row.min + '</td>' + '<td>' + row.max + '</td><td>' + percents + ' % ' + '</td></tr>');
+
+           $(insertedRow).find('td').last().css('color', percents >= 0 ? 'green' : 'red');
 
        });
-
-        console.log(rows);
-
-
     });
-
     return false;
 });
-
 
 
 function fetchData(url,minDate,maxDate) {
