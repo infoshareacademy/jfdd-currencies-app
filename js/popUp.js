1bc09f79dff6e1e9ab46c26132e338357b425ff3
emilia <emilia.kamrowska@o2.pl>
2016-02-11 19:01:51 +0100

    duuuuuuuużo zmian  - logowanie, popup

--- js/popUp.js
+++ js/popUp.js
@@ -1,17 +1,17 @@
-
-    $('.socialMediaLogIn').click( function()    {
-        localStorage.setItem("popupWasShown",1);
-        //location.reload();
-
-            setTimeout(function () {
-                location.reload();
-            }, 2000);
-    });
-
-    (function () {
-    if(localStorage.getItem("popupWasShown") != 1)  {
-        $('#PopUp-wrapper').removeAttr('style', 'display:none');
-    }
-
-    }());
-
+    //
+    //$('.socialMediaLogIn').click( function()    {
+    //    localStorage.setItem("popupWasShown",1);
+    //    //location.reload();
+    //
+    //        setTimeout(function () {
+    //            location.reload();
+    //        }, 2000);
+    //});
+    //
+    //(function () {
+    //if(localStorage.getItem("popupWasShown") != 1)  {
+    //    $('#PopUp-wrapper').removeAttr('style', 'display:none');
+    //}
+    //
+    //}());
+    //
