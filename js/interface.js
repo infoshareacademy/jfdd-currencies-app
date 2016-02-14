16c99930617c71c597d838175353c1a2443cfe3e
siwexx <jmsiwinski@gmail.com>
2016-02-14 22:07:07 +0100

    start-up

--- js/interface.js
+++ js/interface.js
@@ -1,14 +1,5 @@
-/**
- * Created by Tomasz on 19.01.2016.
- */
+
 $(document).ready(function(){
-
-    (function () {
-        if(! localStorage.name)  {
-            $('#PopUp-wrapper').removeAttr('style', 'display:none');
-        }
-
-    }());
 
     $(".trigger-slide").click(function() {
         $('.menu-hidden').toggleClass('menu-show-up')
@@ -23,22 +14,26 @@ $(document).ready(function(){
 
 
     $('.motive-blue-btn').click(function(){
-        $('.blue-skin').css('background-color','#355b9e');
+        $('.blue-skin').css('background-color','#2980B9');
+        $('img').attr('src','images/logo_blue.svg')
     });
 
     $('.motive-red-btn').click(function(){
-        $('.blue-skin').css('background-color','#CC0000');
+        $('.blue-skin').css('background-color','#C0392B');
+        $('img').attr('src','images/logo_red.svg')
     });
 
     $('.motive-green-btn').click(function(){
-        $('.blue-skin').css('background-color','#73AD21');
+        $('.blue-skin').css('background-color','#16A085');
+        $('img').attr('src','images/logo_green.svg')
     });
 
     $(function () {
         $('#userName').append(localStorage.getItem("name"));
-
     });
-
-
 });
+
+var date = new Date();
+var actualYear = date.getFullYear();
+document.getElementById("currentYear").innerHTML = actualYear;
 
